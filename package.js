3c7c55b2f78c84665c763cd1f7224dd1677a0c6e
DuongDuyChuong <duongduychuong10ca2@gmail.com>
2021-09-18 15:46:06 +0700

    cai dat xu ly loi

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ const express =require("express");
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError } = require("./app/helpers/error");
 
 const app = express();
 
@@ -21,9 +22,19 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book applicantion"});
 });
 
-
 const PORT = config.app.port;
 
 app.listen(PORT, () => {
     console.log(`sever is running on port ${PORT}.`);
+});
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
 });
